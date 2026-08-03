155922c40a296176ac1cb77c25feafa068673271
Siddhi Pandharkar <pandharkar_s@xoriant.com>
2026-08-03 12:32:59 +0530

    Initial Maven project setup

---
+++ src/main/java/com/expensetracker/backend/Main.java
@@ -0,0 +1,12 @@
+package com.expensetracker.backend;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println(" Expense Tracker Backend Started ");
+        System.out.println("================================");
+
+    }
+}
