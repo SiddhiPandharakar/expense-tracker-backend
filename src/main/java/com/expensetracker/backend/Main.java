db93936b05f6180814d8082cb88315d5b19f3cec
Siddhi Pandharkar <pandharkar_s@xoriant.com>
2026-08-03 16:11:07 +0530

    feat: add users table schema

--- src/main/java/com/expensetracker/backend/Main.java
+++ src/main/java/com/expensetracker/backend/Main.java
@@ -1,12 +1,23 @@
 package com.expensetracker.backend;
 
+import com.expensetracker.backend.config.DBConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("================================");
-        System.out.println(" Expense Tracker Backend Started ");
-        System.out.println("================================");
-
+        try
+        {
+            Connection connection = DBConnection.getConnection();
+            if(connection != null){
+                System.out.println("Connected");
+                connection.close();
+            }
+        } catch (Exception e) {
+            System.out.println(" Database Connection Failed!");
+            System.out.println(e.getMessage());
+        }
     }
 }
